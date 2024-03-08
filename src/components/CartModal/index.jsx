53a5fb3b615328faf793d864bbe35f73ffbc1b32
bruno-palma <146733523+bruno-palma@users.noreply.github.com>
2024-03-08 13:55:20 -0300

    Continuando a Estilização com SASS - Part 2

--- src/components/CartModal/index.jsx
+++ src/components/CartModal/index.jsx
@@ -1,5 +1,6 @@
 import { MdClose } from "react-icons/md";
 import { CartItemCard } from "./CartItemCard";
+import styles from "./styles.module.scss";
 
 export const CartModal = ({
   cartList,
@@ -9,39 +10,43 @@ export const CartModal = ({
   amount,
 }) => {
   return (
-    <div role="dialog">
-      <div>
-        <h2>Carrinho de compras</h2>
-        <button
-          aria-label="close"
-          title="Fechar"
-          onClick={() => setIsOpen(false)}
-        >
-          <MdClose size={21} />
-        </button>
-      </div>
-      <div>
-        <ul>
-          {cartList.map((product) => (
-            <CartItemCard
-              key={product.id}
-              product={product}
-              removeToCart={removeToCart}
-            />
-          ))}
-        </ul>
-      </div>
-      <div>
-        <div>
-          <span>Total </span>
-          <span>
-            {amount.toLocaleString("pt-BR", {
-              style: "currency",
-              currency: "BRL",
-            })}
-          </span>
+    <div className={styles.modal__overlay} role="dialog">
+      <div className={styles.modal__container}>
+        <div className={styles.modal__top}>
+          <h2>Carrinho de compras</h2>
+          <button
+            aria-label="close"
+            title="Fechar"
+            onClick={() => setIsOpen(false)}
+          >
+            <MdClose size={21} />
+          </button>
         </div>
-        <button onClick={() => removeAll()}>Remover todos</button>
+        <div className={styles.modal__info}>
+          <ul>
+            {cartList.map((product) => (
+              <CartItemCard
+                key={product.id}
+                product={product}
+                removeToCart={removeToCart}
+              />
+            ))}
+          </ul>
+        </div>
+        <div className={styles.modal__bottom}>
+          <div>
+            <span className={styles.total}>Total</span>
+            <span className={styles.amount}>
+              {amount.toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </span>
+          </div>
+          <button className={styles.button} onClick={() => removeAll()}>
+            Remover todos
+          </button>
+        </div>
       </div>
     </div>
   );
