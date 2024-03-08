0f5062bfc6646e3909d26c4d71fdc2673f689dda
bruno-palma <146733523+bruno-palma@users.noreply.github.com>
2024-03-08 17:29:10 -0300

    Finalizada a Estilização com SASS

--- src/components/CartModal/CartItemCard/index.jsx
+++ src/components/CartModal/CartItemCard/index.jsx
@@ -1,22 +1,31 @@
 import { MdDelete } from "react-icons/md";
+import styles from "./styles.module.scss";
 
 export const CartItemCard = ({ product, removeToCart }) => {
   return (
     <li>
-      <div>
-        <div>
+      <div className={styles.cart__container}>
+        <div className={styles.cart__info}>
           <img src={product.img} alt={product.name} />
-          <span>Qtd. {product.quantity}</span>
+          <div>
+            <h3>{product.name}</h3>
+            <span className={styles.price}>
+              {product.price.toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </span>
+            <span className={styles.quantity}>Qtd. {product.quantity}</span>
+          </div>
         </div>
-        <h3>{product.name}</h3>
+        <button
+          aria-label="delete"
+          title="Remover item"
+          onClick={() => removeToCart(product.id)}
+        >
+          <MdDelete size={21} />
+        </button>
       </div>
-      <button
-        aria-label="delete"
-        title="Remover item"
-        onClick={() => removeToCart(product.id)}
-      >
-        <MdDelete size={21} />
-      </button>
     </li>
   );
 };
